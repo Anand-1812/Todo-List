8409907ee9140f558ddbe52a1b603fb6da2a84d8
Anand-1812 <anand.work2005@gmail.com>
2025-05-29 14:09:50 +0530

    complete the add task part

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import "./styles.css";
 import "./sidebar.js";
 import { DomLogs } from "./domLog.js";
 import { createProject, SidebarNav } from "./sidebar.js";
+import { Todo } from "./todos.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   DomLogs.init();
   createProject();
+  Todo.init();
 });
