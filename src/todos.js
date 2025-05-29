ad5f2d6a0b0461f367e2a8f4f2a5853311753213
Anand-1812 <anand.work2005@gmail.com>
2025-05-29 14:08:11 +0530

    for todo dynamically

---
+++ src/todos.js
@@ -0,0 +1,72 @@
+import { DomLogs } from "./domLog";
+import { format } from "date-fns";
+
+export class Todo {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+
+  static init() {
+    DomLogs.addTaskBtn.addEventListener("click", Todo.createTask);
+  }
+
+  static createTask() {
+    const todoInfo = document.createElement("dialog");
+    todoInfo.id = "todo-info";
+
+    const form = document.createElement("form");
+    form.id = "todo-form";
+    form.method = "dialog";
+    form.innerHTML = `
+      <p>Add Todo</P>
+      <p>
+        <label for="title">Title</label>
+        <input type="text" id="title" name="title" autocomplete="off">
+      </p>
+      <p>
+        <label for="description">Description</label>
+        <input type="text" id="description" name="description" autocomplete="off">
+      </p>
+      <p>
+        <label for="date">Due Date</label>
+        <input type="date" id="date" name="date">
+      </P>
+      <p>
+        <label for="priority">Priority</label>
+        <input type="number" id="priority" name="priority" autocomplete="off">
+      </P>
+      <p>
+        <button type="button" class="close-btn">Close</button>
+        <button type="submit" class="submit-btn">Submit</button>
+      </p>
+    `;
+
+    // adding to the dom
+    todoInfo.appendChild(form);
+    document.body.appendChild(todoInfo);
+    todoInfo.showModal();
+
+    // close the dialog
+    form.querySelector(".close-btn").addEventListener("click", () => {
+      todoInfo.close();
+      todoInfo.remove();
+    });
+
+    // handle form submission
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const title = form.title.value;
+      const desc = form.description.value;
+      const date = format(new Date(form.date.value), "yyyy-MM-dd");
+      const priority = form.priority.value;
+
+      const newTodo = new Todo(title, desc, date, priority);
+
+      todoInfo.close();
+      todoInfo.remove();
+    });
+  }
+}
