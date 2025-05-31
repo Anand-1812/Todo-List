a749870138b107fe7818f2fe2711e650d1d54d63
Anand-1812 <anand.work2005@gmail.com>
2025-05-31 15:34:25 +0530

    add delete button and local storage

--- src/todos.js
+++ src/todos.js
@@ -1,6 +1,7 @@
 import { DomLogs } from "./global";
 import { format } from "date-fns";
-import { projects, setCurrentProject } from "./store";
+import deleteIcon from "./assets/images/deleteIcon.svg";
+import { projects, getCurrentProject } from "./store";
 
 export class Todo {
   constructor(title, description, dueDate, priority) {
@@ -11,7 +12,13 @@ export class Todo {
   }
 
   static init() {
+    document.querySelector(".todo-items").innerHTML = "";
     DomLogs.addTaskBtn.addEventListener("click", Todo.createTask);
+    const stored = JSON.parse(localStorage.getItem("myLocalStore")) || [];
+    stored.forEach(([title, desc, date, priority]) => {
+      const todo = new Todo(title, desc, date, priority);
+      Todo.addTodo(todo);
+    })
   }
 
   static createTask() {
@@ -22,7 +29,7 @@ export class Todo {
     form.id = "todo-form";
     form.method = "dialog";
     form.innerHTML = `
-      <p>Add Todo</P>
+      <p>Add Todo</p>
       <p>
         <label for="title">Title</label>
         <input type="text" id="title" name="title" autocomplete="off">
@@ -37,7 +44,7 @@ export class Todo {
       </P>
       <p>
         <label for="priority">Priority</label>
-        <input type="number" id="priority" name="priority" autocomplete="off">
+        <input type="number" id="priority" name="priority" min="1" max="5" autocomplete="off">
       </P>
       <p>
         <button type="button" class="close-btn">Close</button>
@@ -64,9 +71,19 @@ export class Todo {
       const date = format(new Date(form.date.value), "yyyy-MM-dd");
       const priority = form.priority.value;
 
-      const newTodo = new Todo(title, desc, date, priority);
-      projects[setCurrentProject].push(newTodo);
+      // trying local storage
+      const existingTodo = JSON.parse(localStorage.getItem("myLocalStore")) || [];
+      existingTodo.push([title, desc, date, priority]);
+      localStorage.setItem("myLocalStore", JSON.stringify(existingTodo));
+      
 
+      const projectName = getCurrentProject();
+      if (!projects[projectName]) {
+        projects[projectName] = [];
+      }
+
+      const newTodo = new Todo(title, desc, date, priority);
+      projects[projectName].push(newTodo);
       Todo.addTodo(newTodo);
 
       todoInfo.close();
@@ -80,8 +97,17 @@ export class Todo {
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo-card");
 
+    const todoHead = document.createElement("div");
+    todoHead.className = "todo-head";
+
+    const delteTodoBtn = document.createElement("button");
+    delteTodoBtn.className = "delete-todo";
+    delteTodoBtn.innerHTML = `<img src="${deleteIcon}" alt="Delete">`;
+
     const title = document.createElement("h3");
     title.textContent = todo.title;
+
+    todoHead.append(title, delteTodoBtn);
 
     const desc = document.createElement("p"); 
     desc.textContent = todo.description;
@@ -93,8 +119,26 @@ export class Todo {
     priority.classList.add("todo-priority");
     priority.textContent = `Priority: ${todo.priority}`;
 
-    todoDiv.append(title, desc, date, priority);
+    todoDiv.append(todoHead, desc, date, priority);
     itemsContainer.appendChild(todoDiv);
+
+    // delete todo logic......
+    delteTodoBtn.addEventListener("click", () => {
+      todoDiv.remove();
+
+      const stored = JSON.parse(localStorage.getItem("myLocalStore")) || [];
+      const updated = stored.filter(
+        ([t, d, da, p]) => !(t === todo.title && d === todo.description && da === todo.dueDate && p == todo.priority)
+      );
+
+      localStorage.setItem("myLocalStore", JSON.stringify(updated));
+      const currentProject = getCurrentProject();
+      if (projects[currentProject]) {
+        projects[currentProject] = projects[currentProject].filter(
+          t => !(t.title === todo.title && t.description === todo.description && t.dueDate === todo.dueDate && t.priority == todo.priority)
+        );
+      }
+    });
 
   }
 
