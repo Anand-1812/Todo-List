0f8a779ca1583ce9b65b33deede28cb0a103b32a
Anand-1812 <anand.work2005@gmail.com>
2026-01-18 20:23:15 +0530

    input form

--- app/auth/Signup.tsx
+++ app/auth/Signup.tsx
@@ -3,11 +3,23 @@ const Signup = () => {
     <div className="min-h-screen flex flex-col justify-center items-center p-4">
       <div className="w-full max-w-6xl mx-auto bg-neutral-600 flex justify-center">
         <div>Text Div</div>
-        <div>
+
+        <div className="flex flex-col">
           <h2>Let's get you onboard</h2>
+
           <form className="flex flex-col">
-            <label htmlFor="email">Name</label>
-            <input name="email" id="email" placeholder="example@gmail.com" />
+            <p>
+              <label htmlFor="email">Name</label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                placeholder="example@gmail.com"
+                className="rounded-xl border border-neutral-700 px-4 text-white
+                placeholder:text-neutral-400 focus:outline-none focus:ring-1
+                focus:ring-sky-400 transition duration-200"
+              />
+            </p>
           </form>
         </div>
       </div>
